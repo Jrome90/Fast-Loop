12ace2beb4226864f56688605f6e82bccce3f70c
Jrome <jrome613@gmail.com>
2024-07-07 18:39:44 -0700

    version 2.1.0

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ bl_info = {
     'author': 'Jrome',
     'description': 'Quickly insert loop cuts at the mouse position with a preview of the loop cuts.',
     'blender': (4, 0, 0),
-    'version': (2, 0, 6),
+    'version': (2, 1, 0),
     'category': 'Mesh',
 }
 
